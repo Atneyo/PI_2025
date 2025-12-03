8e523fa8c102c169e7bbf84cd81324616979da5b
Chloe Boninn <chloe.boninn@telecomnancy.eu>
2025-12-03 17:15:36 +0100

    Remove unused files

--- demo_modeles/SpeechBrain_test.py
+++
@@ -1,8 +0,0 @@
-from speechbrain.pretrained import EncoderDecoderASR
-
-asr = EncoderDecoderASR.from_hparams(
-    source="speechbrain/asr-crdnn-rnnlm-librispeech",
-    savedir="tmp_asr",
-    run_opts={"device":"cpu"}  # ou "cuda" si GPU
-)
-print(asr.transcribe_file("test_poeme_anglais.wav"))
