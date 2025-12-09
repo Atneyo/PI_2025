c8411bdbb6b5de35e6574cb51f91d1ba6c3c6aff
Chloe Boninn <chloe.boninn@telecomnancy.eu>
2025-12-09 10:59:16 +0100

    create main.py for backend and add multiple files

---
+++ interface/backend/main.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile | None = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        return {"filename": file.filename}
+    
+@app.post("/uploadfiles/")
+async def create_upload_files(files: list[UploadFile]):
+    return {"filenames": [file.filename for file in files]}
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
