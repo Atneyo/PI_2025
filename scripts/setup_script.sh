e94122485a944f14ce2610e25d618ceca33e999f
Cenedra890 <cenedra.belgarad@gmail.com>
2025-12-18 10:48:05 +0100

    Added docker installation to the setup script

--- scripts/setup_script.sh
+++ scripts/setup_script.sh
@@ -2,18 +2,18 @@
 set -e
 
 ### System Update & Prerequisites
-echo "[1/10] Updating system"
+echo "[1/11] Updating system"
 apt update
 apt upgrade -y
 apt full-upgrade -y
 
-echo "[2/10] Installing development tools"
+echo "[2/11] Installing development tools"
 apt install -y python3-pip ffmpeg git curl cmake build-essential code # code to be removed in the final version of the script (used for debugging/development on the raspberry pi)
 
-echo "[3/10] Installing libraries"
+echo "[3/11] Installing libraries"
 apt install -y libx264-dev libjpeg-dev
 
-echo "[4/10] Installing GStreamer stack"
+echo "[4/11] Installing GStreamer stack"
 apt install -y \
     libgstreamer1.0-dev \
     libgstreamer-plugins-base1.0-dev \
@@ -27,12 +27,12 @@ apt install -y \
 
 
 ### Bootloader / EEPROM Update
-echo "[5/10] Updating Raspberry Pi EEPROM"
+echo "[5/11] Updating Raspberry Pi EEPROM"
 rpi-eeprom-update -a || true
 
 
 ### Enable PCIe Gen 3.0 
-echo "[6/10] Enabling PCIe Gen 3.0"
+echo "[6/11] Enabling PCIe Gen 3.0"
 
 CONFIG_FILE="/boot/firmware/config.txt"
 if ! grep -q "^dtparam=pciex1_gen=3" "$CONFIG_FILE"; then
@@ -41,7 +41,7 @@ fi
 
 
 ### Enable Latest Bootloader
-echo "[7/10] Setting bootloader to latest stable"
+echo "[7/11] Setting bootloader to latest stable"
 BOOTCONF="/etc/default/rpi-eeprom-update"
 
 if ! grep -q "FIRMWARE_RELEASE_STATUS=stable" "$BOOTCONF"; then
@@ -76,7 +76,7 @@ detect_hailo_hat() {
 
 
 ### Conditional Hailo Installation
-echo "[8/10] Checking for Hailo HAT"
+echo "[8/11] Checking for Hailo HAT"
 
 if detect_hailo_hat; then
     echo "Installing Hailo packages"
@@ -89,11 +89,37 @@ fi
 
 
 ### Node.js and npm
-echo "[9/10] Installing Node.js and npm"
+echo "[9/11] Installing Node.js and npm"
 apt install -y nodejs npm
 
 
+### Docker installation following this guide: https://docs.docker.com/engine/install/debian/
+echo "[10/11] Installing Docker"
+apt remove $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1)
+# Add Docker's official GPG key:
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo trixie) 
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
+
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+
+
 ### Final Reboot
-echo "[10/10] Installation process completed. Rebooting device to apply changes"
+echo "[11/11] Installation process completed. Rebooting device to apply changes"
 sleep 5
 reboot
