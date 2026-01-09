e7550696567b9986e7db2636c203f7e30bae2e46
Cenedra890 <cenedra.belgarad@gmail.com>
2026-01-09 11:47:12 +0100

    added joularcore tool installation to setup script

--- scripts/setup_script.sh
+++ scripts/setup_script.sh
@@ -2,18 +2,18 @@
 set -e
 
 ### System Update & Prerequisites
-echo "[1/11] Updating system"
+echo "[1/12] Updating system"
 apt update
 apt upgrade -y
 apt full-upgrade -y
 
-echo "[2/11] Installing development tools"
+echo "[2/12] Installing development tools"
 apt install -y python3-pip ffmpeg git curl cmake build-essential code # code to be removed in the final version of the script (used for debugging/development on the raspberry pi)
 
-echo "[3/11] Installing libraries"
+echo "[3/12] Installing libraries"
 apt install -y libx264-dev libjpeg-dev
 
-echo "[4/11] Installing GStreamer stack"
+echo "[4/12] Installing GStreamer stack"
 apt install -y \
     libgstreamer1.0-dev \
     libgstreamer-plugins-base1.0-dev \
@@ -27,12 +27,12 @@ apt install -y \
 
 
 ### Bootloader / EEPROM Update
-echo "[5/11] Updating Raspberry Pi EEPROM"
+echo "[5/12] Updating Raspberry Pi EEPROM"
 rpi-eeprom-update -a || true
 
 
 ### Enable PCIe Gen 3.0 
-echo "[6/11] Enabling PCIe Gen 3.0"
+echo "[6/12] Enabling PCIe Gen 3.0"
 
 CONFIG_FILE="/boot/firmware/config.txt"
 if ! grep -q "^dtparam=pciex1_gen=3" "$CONFIG_FILE"; then
@@ -41,7 +41,7 @@ fi
 
 
 ### Enable Latest Bootloader
-echo "[7/11] Setting bootloader to latest stable"
+echo "[7/12] Setting bootloader to latest stable"
 BOOTCONF="/etc/default/rpi-eeprom-update"
 
 if ! grep -q "FIRMWARE_RELEASE_STATUS=stable" "$BOOTCONF"; then
@@ -76,7 +76,7 @@ detect_hailo_hat() {
 
 
 ### Conditional Hailo Installation
-echo "[8/11] Checking for Hailo HAT"
+echo "[8/12] Checking for Hailo HAT"
 
 if detect_hailo_hat; then
     echo "Installing Hailo packages"
@@ -89,12 +89,12 @@ fi
 
 
 ### Node.js and npm
-echo "[9/11] Installing Node.js and npm"
+echo "[9/12] Installing Node.js and npm"
 apt install -y nodejs npm
 
 
 ### Docker installation following this guide: https://docs.docker.com/engine/install/debian/
-echo "[10/11] Installing Docker"
+echo "[10/12] Installing Docker"
 apt remove $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1)
 # Add Docker's official GPG key:
 apt update
@@ -117,9 +117,20 @@ apt update
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
+#Joularcore installation (for energy monitoring)
+echo "[11/12] installing joular core"
+git clone https://github.com/joular/joularcore.git
+cd joularcore
+apt install rustup
+rustup default stable
+cargo build --release
+cp target/release/joularcore /usr/local/bin/joularcore
+cd ..
+
+
 
 
 ### Final Reboot
-echo "[11/11] Installation process completed. Rebooting device to apply changes"
+echo "[12/12] Installation process completed. Rebooting device to apply changes"
 sleep 5
 reboot
